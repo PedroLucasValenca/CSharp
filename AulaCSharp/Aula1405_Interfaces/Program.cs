0ba3189807ceac6dd01c01d5267f642b38a6ea59
Pedro Lucas Rennó Valença <pedrolucas.valenca@protonmail.com>
2018-05-14 21:59:11 -0300

    implementando injeção de dependencia  polimorfismo

--- AulaCSharp/Aula1405_Interfaces/Program.cs
+++ AulaCSharp/Aula1405_Interfaces/Program.cs
@@ -10,6 +10,35 @@ namespace Aula1405_Interfaces
     {
         static void Main(string[] args)
         {
+            Carro c = new Carro();
+
+            Console.WriteLine("Carro Criado, velocidade Inicial: " + c.Velocidade);
+            c.Acelerar();
+            c.Acelerar();
+            Console.WriteLine("Carro, velocidade Atual: " + c.Velocidade);
+            AcelerarBastante(c);
+            Console.WriteLine("Carro, velocidade Atual: " + c.Velocidade);
+
+            Onibus o = new Onibus();
+            Console.WriteLine("Onibus Criado, velocidade Atual: " + o.Velocidade);
+            o.Acelerar();
+            o.Acelerar();
+            Console.WriteLine("Onibus, velocidade Atual: " + o.Velocidade);
+            AcelerarBastante(o);
+            Console.WriteLine("Onibus, velocidade Atual: " + o.Velocidade);
+
+            Console.ReadKey();
+           
+        }
+        static void AcelerarBastante(IMeioTransporte meioTransporte)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                meioTransporte.Acelerar();
+
+            }
         }
     }
+
+    
 }
